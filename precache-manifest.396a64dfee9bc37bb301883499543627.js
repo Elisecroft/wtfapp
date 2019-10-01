503166287ee7de41c085babaf667d69085851b23
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-01 13:07:21 +0000

    Deploy HenriqueMisael/wtfapp to github.com/HenriqueMisael/wtfapp.git:gh-pages

---
+++ precache-manifest.396a64dfee9bc37bb301883499543627.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57559e271cce9e451a7b5dc29ef0dc09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e4a795aeab29cc5db5a",
+    "url": "/static/css/main.bbc7d2bd.chunk.css"
+  },
+  {
+    "revision": "dde995bd48e0e6c45f9f",
+    "url": "/static/js/2.cdcb6f01.chunk.js"
+  },
+  {
+    "revision": "7e4a795aeab29cc5db5a",
+    "url": "/static/js/main.1d8c3976.chunk.js"
+  },
+  {
+    "revision": "d0e6f7f7012754453b8f",
+    "url": "/static/js/runtime~main.60e57fc4.js"
+  }
+]);
